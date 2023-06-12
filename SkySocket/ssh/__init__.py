cfeb176ae201c6dcb8179bfa04c9b381b311d20d
Demetrius Johnson <107090794+meechtheballer99@users.noreply.github.com>
2023-06-12 10:54:41 -0400

    questionable discisions

--- SkySocket/ssh/__init__.py
+++ SkySocket/ssh/__init__.py
@@ -1,28 +1,88 @@
 from fabric import Connection
+DRONE_HOSTNAME = '192.168.11.1'
+DRONE_USER = 'pi'             # Hostname
+DRONE_SUDO_PASS = 'raspberry'        # sudo password
+CAR_HOSTNAME= '192.168.11.133'
+CAR_USER = 'pi'
+CAR_SUDO_PASS = 'raspberry'
 
-DRONE_HOSTNAME = ''         # IP
-DRONE_USER = ''             # Hostname
-DRONE_SUDO_PASS = ''        # sudo password
-CAR_HOSTNAME = ''
-CAR_USER = ''
-CAR_SUDO_PASS = ''
+DRONE_DIR = 'python_files/sd1_demo'
+DRONE_FILE = 'RedDetect_drone-SD1_demo-v4.py'
+CAR_DIR = 'python_files/sd1_demo'
+CAR_FILE = 'car_server.py'
 
-SERVER_DIR = '/home/pi/Python_files/SocketPrograms'
-SERVER_FILE = 'server.py'
-CLIENT_DIR = '/home/pi/'
-CLIENT_FILE = 'RedDetect.py'
+SHITTY_NUMBER = 0
 
 def connect():
     c = Connection(host=DRONE_HOSTNAME, user=DRONE_USER, connect_kwargs = {"password": DRONE_SUDO_PASS})
     c.run('pwd')
     c.close()
 
-def start_experiment():
-    car_con = Connection(host=CAR_HOSTNAME, user=CAR_USER, connect_kwargs = {"password": CAR_SUDO_PASS})
-    car_con.run(f'cd {SERVER_DIR}')
-    car_con.run(f'python3 {SERVER_FILE}')
-    car_con.close()
-    drone_con = Connection(host=DRONE_HOSTNAME, user=DRONE_USER, connect_kwargs = {"password": DRONE_SUDO_PASS})
-    drone_con.run(f'cd {CLIENT_DIR}')
-    drone_con.run(f'python3 {CLIENT_FILE}')
-    drone_con.close()
+def SHITTY_BUTTON():
+    global SHITTY_NUMBER
+    if SHITTY_NUMBER%2 == 0:
+        CAR.init(CAR_HOSTNAME, CAR_USER, CAR_SUDO_PASS)
+        CAR.start_experiment(CAR_DIR, CAR_FILE)
+        print('CAR THING RUN')
+        DRONE.init(DRONE_HOSTNAME, DRONE_USER, DRONE_SUDO_PASS)
+        DRONE.start_experiment(DRONE_DIR, DRONE_FILE)
+        print('DRONE THING RUN')
+        SHITTY_NUMBER = SHITTY_NUMBER+1
+    else:    
+        CAR.end()
+        DRONE.end()
+        SHITTY_NUMBER =SHITTY_NUMBER+1
+
+def car_init():
+    CAR.init(CAR_HOSTNAME, CAR_USER, CAR_SUDO_PASS)
+    print("CAR ESTABLISHED")
+
+def car_begin():
+    try:
+        CAR.start_experiment(CAR_DIR, CAR_FILE)
+        print("CAR READY")
+    except:
+        print("CAR FAILED")
+
+def drone_init():
+    DRONE.init(DRONE_HOSTNAME, DRONE_USER, DRONE_SUDO_PASS)
+    print("DRONE ESTABLISHED")
+
+def drone_begin():
+    try:
+        DRONE.start_experiment(DRONE_DIR, DRONE_FILE)
+        print("DRONE READY")
+    except:
+        print("DRONE FAILED")
+
+def stop():
+    CAR.end()
+    DRONE.end()
+    print("EXPERIMENT DONE")
+    
+
+class SSH():
+    def init(self, hostname, user, password):
+        self._hostname = hostname
+        self._user = user
+        self._password = password
+        self.con = Connection(host=self._hostname, user=self._user, connect_kwargs = {"password": self._password})
+    
+    def start_experiment(self, dir_path, file_path):
+        self.file = file_path
+        self.con.run(f'python3 {dir_path}/{file_path}')
+
+    def end(self):
+        self.con.run(f'killall {self.file}')
+        self.con.close()
+
+
+CAR = SSH()
+DRONE = SSH()
+
+if __name__ == '__main__':
+    c = SSH()
+    c.init(CAR_HOSTNAME, CAR_USER, CAR_SUDO_PASS)
+    c.start_experiment(CAR_DIR, CAR_FILE)
+    input()
+    c.end()
