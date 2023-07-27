5a5843115a7d8b682e2898390d7ddb1bcb16b7bb
meechtheballer99 <107090794+meechtheballer99@users.noreply.github.com>
2023-07-26 21:08:35 -0400

    Update Edge_Server_VideoTopic.py
    updated logging with fixes

--- SD2-FinalDemo-August/Edge_Server_VideoTopic.py
+++ SD2-FinalDemo-August/Edge_Server_VideoTopic.py
@@ -1,6 +1,7 @@
 # This code is used to start the edge server video topic so that the edge server can subscribe to this topic
 # via a web interface and process frames in YOLOv4. This will allow a load balance mechanism
 # so that the edge server only receives frames not processed by the drone for object detection.
+# This module should be imported by Red_and_Edge_detect program
 
 import rospy
 from sensor_msgs.msg import Image
@@ -11,23 +12,30 @@ SUBSCRIBER_TOPIC = 'main_camera/image_raw'
 PUBLISHER_TOPIC = 'EdgeServer_VideoTopic/image_raw'
 
 
+# Clear log file (by opening in write mode) before reopening in append mode
+with open("red_and_edge_object_detection_log.txt", mode="w"):
+    pass
+
+# Create a logger instance
+EdgeServer_video_logger = logging.getLogger(__name__)
+
 # Configure logging to write to a log file and console
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    handlers=[
-        logging.FileHandler("red_and_edge_object_detection_log.txt"),  # Log messages to a file
-        logging.StreamHandler()  # Log messages to the console
-    ]
-)
+EdgeServer_video_logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - [%(name)s] - %(levelname)s - %(message)s")
+file_handler = logging.FileHandler("red_and_edge_object_detection_log.txt", mode="a") # now open in append mode
+file_handler.setFormatter(formatter)
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+EdgeServer_video_logger.addHandler(file_handler)
+EdgeServer_video_logger.addHandler(stream_handler)
 
 def initialize_ros_node():
     try:
         rospy.init_node('EdgeServer_VideoTopic')  # Initialize the ROS node with the name 'EdgeServer_VideoTopic'
-        logging.info("ROS node 'EdgeServer_VideoTopic' initialized.")
+        EdgeServer_video_logger.info("ROS node 'EdgeServer_VideoTopic' initialized.")
         return True
     except rospy.ROSInitException as e:
-        logging.error("Failed to initialize ROS EdgeServer_VideoTopic node: %s", str(e))
+        EdgeServer_video_logger.error("Failed to initialize ROS EdgeServer_VideoTopic node: %s", str(e))
         return False
 
 def image_callback(data):
@@ -35,7 +43,7 @@ def image_callback(data):
         image_pub.publish(data)  # Publish the received image data to the 'EdgeServer_VideoTopic/image_raw' topic
 
     except Exception as e:
-        logging.error("Error in image_callback: %s", str(e))
+        EdgeServer_video_logger.error("Error in image_callback: %s", str(e))
 
 def start_image_subscriber():
     try:
@@ -43,7 +51,7 @@ def start_image_subscriber():
         rospy.spin()  # Keeps the program running until the node is shut down --> change this line here to stop continuous sub and posting
 
     except rospy.ROSException as e:
-        logging.error("ROSException in start_image_subscriber: %s", str(e))
+        EdgeServer_video_logger.error("ROSException in start_image_subscriber: %s", str(e))
 
 def main():
     if not initialize_ros_node():
@@ -51,13 +59,13 @@ def main():
 
     global image_pub
     image_pub = rospy.Publisher(PUBLISHER_TOPIC, Image, queue_size=10)  # Create a publisher for the 'EdgeServer_VideoTopic/image_raw' topic
-    logging.info("ROS publisher created for %s topic.", PUBLISHER_TOPIC)
+    EdgeServer_video_logger.info("ROS publisher created for %s topic.", PUBLISHER_TOPIC)
 
     start_image_subscriber()  # Start the image subscriber to process received frames
 
 if __name__ == "__main__":
-    logging.info("Attempting to publish EdgeServer_VideoTopic as the main program (not as an imported program)")
+    EdgeServer_video_logger.info("Attempting to publish EdgeServer_VideoTopic as the main program (not as an imported program)")
     main()  # Run the main function if this script is executed directly
 else:
-    logging.info("%s module imported...Attempting to publish EdgeServer_VideoTopic", __name__)
-    main()  # Run the main function if this script is imported as a module
+    EdgeServer_video_logger.info("%s module imported...Attempting to publish EdgeServer_VideoTopic", __name__)
+    main()  # still run the main function if this script is imported as a module
