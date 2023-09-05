401c31d1b782cc9deb2d0ac171f600249cebbc5f
meechtheballer99 <107090794+meechtheballer99@users.noreply.github.com>
2023-09-05 00:16:59 -0400

    added ping rtt script

--- SD2-FinalDemo-August/GetDroneBatteryInfo.py
+++ SD2-FinalDemo-August/GetDroneBatteryInfo.py
@@ -1,23 +1,69 @@
-from selenium import webdriver
-import time
+#!/usr/bin/env python
 
-# URL of the webpage
-url = "http://192.168.11.1/clover/topics.html?topic=/mavros/battery"
+import rospy
+from sensor_msgs.msg import BatteryState
+import csv
+import datetime
 
-# Initialize a WebDriver (make sure you have the appropriate WebDriver installed)
-driver = webdriver.Chrome()  # You can replace 'Chrome' with the browser of your choice
+csv_filename = f'battery_data_{datetime.datetime.now():%Y-%m-%d_%H-%M-%S}.csv'
 
-# Navigate to the webpage
-driver.get(url)
+def battery_callback(data):
+    with open(csv_filename, mode='a') as file:
+        writer = csv.writer(file)
+        
+        if file.tell() == 0:  # Write header row if the file is empty
+            field_names = [
+                'Seq',
+                'Stamp_secs',
+                'Stamp_nsecs',
+                'Frame_ID',
+                'Voltage',
+                'Temperature',
+                'Current',
+                'Charge',
+                'Capacity',
+                'Design_Capacity',
+                'Percentage',
+                'Power_Supply_Status',
+                'Power_Supply_Health',
+                'Power_Supply_Technology',
+                'Present',
+                'Cell_Voltage_1',
+                'Cell_Voltage_2',
+                'Cell_Voltage_3',
+                'Cell_Voltage_4'
+            ]
+            writer.writerow(field_names)
+        
+        writer.writerow([
+            data.header.seq,
+            data.header.stamp.secs,
+            data.header.stamp.nsecs,
+            data.header.frame_id,
+            data.voltage,
+            data.temperature,
+            data.current,
+            data.charge,
+            data.capacity,
+            data.design_capacity,
+            data.percentage,
+            data.power_supply_status,
+            data.power_supply_health,
+            data.power_supply_technology,
+            data.present,
+            *data.cell_voltage
+        ])
 
-# Wait for the content to load (you might need to adjust the time)
-time.sleep(5)
+def status_timer_callback(event):
+    rospy.loginfo("Program is running and receiving messages.")
 
-# Get the page source after waiting for it to load
-page_source = driver.page_source
-
-# Close the WebDriver when done
-driver.quit()
-
-# Now you can parse the page_source using BeautifulSoup or any other parsing method
-# to extract the necessary data from the dynamically loaded content.
+if __name__ == '__main__':
+    rospy.init_node('battery_subscriber', anonymous=True)
+    
+    # Set up the subscriber
+    rospy.Subscriber('/mavros/battery', BatteryState, battery_callback)
+    
+    # Set up the status timer (5 seconds interval)
+    status_timer = rospy.Timer(rospy.Duration(5.0), status_timer_callback)
+    
+    rospy.spin()  # Keeps the script running until the node is shut down
